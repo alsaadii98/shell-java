4c13f17d1b8bd39f162b09e78f6687143716d244
Ali Abdulhadi <a.h.alsaady1998@gmail.com>
2024-06-27 21:08:08 +0300

    pass 4th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println(input + ": command not found");
             System.out.print("$ ");
             input = scanner.nextLine();
-        } while (!input.matches(""));
+        } while (!input.matches("exit 0"));
         scanner.close();
 
     }
