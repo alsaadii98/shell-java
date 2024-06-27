f5a0576197550da00681098f638a68a5ac69b631
Ali Abdulhadi <a.h.alsaady1998@gmail.com>
2024-06-27 21:04:55 +0300

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()) {
-            String input = scanner.nextLine();
+        String input = scanner.nextLine();
+        do {
             System.out.println(input + ": command not found");
-        }
+            System.out.print("$ ");
+            input = scanner.nextLine();
+        } while (!input.matches(""));
+        scanner.close();
+
     }
 }
